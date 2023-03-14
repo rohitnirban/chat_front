4e17e1545632f96d7e6ce0ba3a8896c6b0b296a8
CodingWithROY <developer.royad@gmail.com>
2023-03-14 21:51:13 +0530

    done 2

--- src/appContext.js
+++ src/appContext.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 import React from "react";
-const SOCKET_URL = "http://back.royad.ga";
+const SOCKET_URL = "https://back.royad.ga";
 export const socket = io(SOCKET_URL);
 // app context
 export const AppContext = React.createContext();
